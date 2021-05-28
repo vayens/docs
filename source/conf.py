ea28fcd349b9f5776ef184a28b5ae4009edf7af7
SunFounder <support@sunfounder.com>
2021-05-28 16:39:33 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -52,7 +52,7 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 import sphinx_rtd_theme
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 from recommonmark.parser import CommonMarkParser
